efc04a45021d31dd955fa55da313209ae861805f
yeojinyun <sail041@naver.com>
2022-07-18 13:34:56 +0900

    button action

--- PhotyPicker/PhotyPicker/ViewController.swift
+++ PhotyPicker/PhotyPicker/ViewController.swift
@@ -13,13 +13,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setConstraint()
+        setDetail()
+        addTarget()
+    }
+    @objc func buttonTapped(_ sender: UIButton) {
+        let nextVC = PickerViewController()
+        let nav = UINavigationController(rootViewController: nextVC)
+        nav.modalPresentationStyle = .fullScreen
+        self.present(nav, animated: true)
+    }
+}
+
+extension ViewController {
+    func addTarget() {
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    }
+    
+    func setDetail() {
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("사진 선택", for: .normal)
+        button.backgroundColor = .black
     }
     
     func setConstraint() {
         view.addSubview(button)
         button.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 200)
+        ])
     }
-
 }
-
---
+++ PhotyPicker/PhotyPicker/pickerView.swift
@@ -0,0 +1,8 @@
+//
+//  pickerView.swift
+//  PhotyPicker
+//
+//  Created by 순진이 on 2022/07/18.
+//
+
+import Foundation
