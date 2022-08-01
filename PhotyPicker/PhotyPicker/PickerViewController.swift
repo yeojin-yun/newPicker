302c9238b368feecc1f973312829b630cfea6a6c
yeojinyun <sail041@naver.com>
2022-08-01 16:33:32 +0900

    remove comment'

--- PhotyPicker/PhotyPicker/PickerViewController.swift
+++ PhotyPicker/PhotyPicker/PickerViewController.swift
@@ -78,8 +78,6 @@ extension PickerViewController: UICollectionViewDataSource, CellDelegate {
     func didPressCheckButton(for index: Int, asset: PHAsset) {
         print("🍎🍎\(index)")
         print("🍎\(asset)")
-        print("🍎\(asset)")
-        print("🍎\(asset)")
         
         
         viewModel.selectedAsset.append(asset)
